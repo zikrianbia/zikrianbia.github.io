c078c8adf264131b1f8e1fce2f470db89a9e17b7
Zikri Anbia <zikri@jobtech.sg>
2025-05-21 12:38:22 +0700

    remove next folder

--- app/constants/projects.ts
+++ app/constants/projects.ts
@@ -3,33 +3,33 @@ import { Project } from "../types";
 // TODO: Move this to API
 export const PROJECTS: Project[] = [
   {
-    title: 'Medium Article',
+    title: 'Medium',
     date: 'Aug 2020',
-    subtext: 'My Medium post on practical QA strategies and automation tips.',
-    url: 'https://github.com/zikrianbia',
+    subtext: 'My Medium post on practical QA strategies and stories.',
+    url: 'https://ziksea.medium.com/',
   },
   {
-    title: 'Travel Guide Web App',
+    title: 'Travel Guide',
     date: 'May 2025',
     subtext: 'I built a website to help travelers discover and book guided tours easily.',
     url: 'https://bintantraveling.netlify.app/',
-  },
-  {
-    title: 'Go-Pro Podcast Guide',
-    date: 'June 2025',
-    subtext: 'A simple site to set up high-quality audio podcasts without overspending.',
-    url: 'https://github.com/zikrianbia/',
-  },
-  {
-    title: 'Edithy',
-    date: 'June 2025',
-    subtext: 'I cut, sync, and bring stories to life.',
-    url: 'https://instagram.com/ziksq/',
   },
   {
     title: 'Testhy',
     date: 'June 2025',
     subtext: 'I turn real-life lessons, weird facts, and tips into fun, story-driven videos.',
     url: 'https://instagram.com/ziksq/',
+  },
+  {
+    title: 'Mixviews',
+    date: 'June 2025',
+    subtext: 'I cut, sync, and bring stories to life.',
+    url: 'https://instagram.com/mixviews/',
+  },
+  {
+    title: 'Audios',
+    date: 'June 2025',
+    subtext: 'A simple site to set up high-quality audio mic without overspending.',
+    url: 'https://github.com/zikrianbia/',
   },
 ];
